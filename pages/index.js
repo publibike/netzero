533941f3a579df07c86ab5c7d5fa04012be46180
johanburgos <johan.burgos@evolutecc.co>
2024-01-17 14:13:44 -0500

    udpate login

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export const index = () => {
 
   const onSubmit = (data) => {
     setLoading(true);
-    mutation.mutate(data);
+    router.push("/home");
   };
 
   return (
@@ -53,11 +53,10 @@ export const index = () => {
           <div className="flex flex-col items-center gap-10">
             <p className="text-[18px] text-[#707070]">
               Bienvenido a{" "}
-              <span style={{ fontWeight: "bold" }}>Comunidades Uflou</span>
+              <span style={{ fontWeight: "bold" }}>NET ZERO</span>
             </p>
-            <img src="img/logo-uflou.png" alt="logo" className="w-[170px]" />
           </div>
-          <div className="w-[50%]">
+          <div className="w-[100%]">
             <div className="w-full">
               <div className="my-2 mx-auto  w-full justify-center flex flex-col gap-10 items-center">
                 <form
@@ -107,14 +106,6 @@ export const index = () => {
                       {errors.password.message}
                     </p>
                   )}
-                  <Link href="/passwordrecover">
-                    <p
-                      target="_blank"
-                      className="m-auto text-[18px] underline text-[#01D9AD]"
-                    >
-                      Olvidé mi contraseña
-                    </p>
-                  </Link>
 
                   <input
                     type="submit"
@@ -123,86 +114,6 @@ export const index = () => {
                   />
                 </form>
               </div>
-            </div>
-          </div>
-          <div className="text-center flex flex-col gap-2">
-            <p className="text-[18px] text-[#707070]">
-              ¿Estás interesado en hacer parte de <strong>Uflou?</strong>
-            </p>
-            <a
-              href="https://api.whatsapp.com/send?phone=573186931271&text=Hola,uFlou"
-              target="_blank"
-              className="text-[18px] underline text-[#01D9AD]"
-            >
-              ¡Escríbenos ahora!
-            </a>
-          </div>
-          <div className="flex flex-col gap-2 text-center">
-            <div className="flex flex-row gap-4">
-              <a target="_blank" href="https://www.instagram.com/uflouapp/">
-                <img
-                  src="icons/instagram.svg"
-                  alt="google"
-                  className="w-[45px] border border-[#707070] rounded-full p-2"
-                />
-              </a>
-              <a target="_blank" href="https://www.facebook.com/uflouapp">
-                <img
-                  src="icons/facebook.svg"
-                  alt="google"
-                  className="w-[45px] border border-[#707070] rounded-full p-2"
-                />
-              </a>
-              <a
-                target="_blank"
-                href="https://www.youtube.com/channel/UCiPQKFUpriLTrRFI9V963wg/videos "
-              >
-                <img
-                  src="icons/youtube.svg"
-                  alt="google"
-                  className="w-[45px] border border-[#707070] rounded-full p-2"
-                />
-              </a>
-            </div>
-            <a
-              href="https://www.ecoapps.com.co/"
-              target="_blank"
-              className="text-[18px] hover:underline text-[#8D8D8D] hover:text-[#00FFCB]"
-            >
-              www.ecoapps.com.co
-            </a>
-          </div>
-        </div>
-        <div className="flex justify-center w-[60%] h-full">
-          <div className="w-full h-full bg-[#01D9AD] rounded-2xl relative">
-            <video
-              autoPlay
-              playsinline
-              loop
-              data-wf-ignore="true"
-              muted
-              className="w-full h-full object-cover rounded-2xl"
-              poster="img/bg-uflou2.png"
-            >
-              <source src="video/bg-uflou2.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-            <div
-              className="absolute top-[3rem] left-[3rem] text-white text-[18px]
-              p-4"
-            >
-              <p className="text-[#00FFCB] text-[44px]">Creamos comunidades</p>
-              <p className="text-[81px] w-[90%] leading-[1em]">
-                por la movilidad sostenible
-              </p>
-              <button
-                onClick={() =>
-                  window.open("https://www.ecoapps.com.co/", "_blank")
-                }
-                className="btn-bg text-[18px] w-[200px] h-[50px] rounded-2xl mt-4"
-              >
-                Conoce más
-              </button>
             </div>
           </div>
         </div>
